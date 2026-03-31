964c90550a6d6e5860d58011bf259e1f50361ba5
mue76 <Ghyunghee@gmail.com>
2026-03-31 10:10:19 +0900

    fix: CSRF_TRUSTED_ORIGINS 환경변수 설정 추가

--- web/config/settings.py
+++ web/config/settings.py
@@ -13,6 +13,8 @@ DEBUG = os.getenv("DJANGO_DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost 127.0.0.1").split()
 
+CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS", "http://localhost:8000").split()
+
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
